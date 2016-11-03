b7c884c723e3f58ffcb10450f6f7b1b3741917b9
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-03 18:48:48 +0100

    Tried if the code works

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ let card = function (x,y) {
   this.width = 70;
 }
 
-// define card constructor function
+// Define card constructor function
 
 let cards = [];
 
-// initializing an empty array
+// Initializing an empty array
 
 let NUM_COLS = 4;
 let NUM_ROWS = 4;
 
-// defines how many columns/rows we want
+// Defines how many columns/rows we want
 
 for (let i = 0; i < NUM_COLS; i++) {
   for (let a = 0; a < NUM_ROWS; a++) {
@@ -21,4 +21,15 @@ for (let i = 0; i < NUM_COLS; i++) {
   }
 }
 
-// outer loop iterates for as many columns as we want, our inner loop iterates for each of the rows
+// Two for loops that iterates how many columns/rows we want
+
+card.prototype.cardFaceDown = function () {
+  fill (214, 247, 202);
+  strokeWeight(2);
+  rect(this.x, this.y, this.width, this.width, 10);
+  image(getImage("images/china-wall.png"), this.x, this.y, this.width, this.width);
+};
+
+for (let i = 0 ; i < cards.length; i++) {
+  cards[i].cardFaceDown();
+};
