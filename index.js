be6c2028a3dace055c9d6e083e1ab6b5f611a89a
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-03 18:39:11 +0100

    Created two for loops that iterates how many columns/rows we want

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let card = function (x,y) {
 
 // define card constructor function
 
-let card = [];
+let cards = [];
 
 // initializing an empty array
 
@@ -14,3 +14,11 @@ let NUM_COLS = 4;
 let NUM_ROWS = 4;
 
 // defines how many columns/rows we want
+
+for (let i = 0; i < NUM_COLS; i++) {
+  for (let a = 0; a < NUM_ROWS; a++) {
+    cards.push(new card(i * 78 + 10, a * 78 + 10));
+  }
+}
+
+// outer loop iterates for as many columns as we want, our inner loop iterates for each of the rows
