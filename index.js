485970f59eeeb09a8a8795832d8962c505ef8878
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-03 18:33:53 +0100

    Defines how many columns/rows we want

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ let card = function (x,y) {
 }
 
 // define card constructor function
+
+let card = [];
+
+// initializing an empty array
+
+let NUM_COLS = 4;
+let NUM_ROWS = 4;
+
+// defines how many columns/rows we want
