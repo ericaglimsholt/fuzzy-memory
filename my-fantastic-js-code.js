932849bd4c3874988e2e82859fff94153bdaff57
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-05 15:19:35 +0100

    Created array of the images

--- my-fantastic-js-code.js
+++ my-fantastic-js-code.js
@@ -7,3 +7,25 @@ let two;
 let hit = 0;
 //if the cards doesn't match back will show
 let back = "images/back.png";
+
+//array of all the possible images
+let imgArray = [
+"images/1.png",
+"images/2.png",
+"images/3.png",
+"images/4.png",
+"images/5.png",
+"images/6.png",
+"images/7.png",
+"images/8.png",
+
+//twice for the matching pare
+"images/1.png",
+"images/2.png",
+"images/3.png",
+"images/4.png",
+"images/5.png",
+"images/6.png",
+"images/7.png",
+"images/8.png",
+];
